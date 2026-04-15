1afd4ed627614e96739e30e5d5edf4d1b66613cd
Sam Mazer <smazer@users.noreply.github.com>
2026-04-14 20:10:43 -0700

    adding debug statements in register types

--- cesium_godot/register_types.cpp
+++ cesium_godot/register_types.cpp
@@ -28,13 +28,16 @@ using namespace godot;
 
 
 void initialize_cesium_godot_module(ModuleInitializationLevel p_level) {
-	//We will probably have to switch the module initialization level to the editor
-	//But we will keep it to the Scene for some testing that the library has been properly linked
+	printf("[CESIUM] initialize_cesium_godot_module called with level %d\n", (int)p_level);
+	fflush(stdout);
 	if (p_level != ModuleInitializationLevel::MODULE_INITIALIZATION_LEVEL_SCENE)
 		return;
-	//We will have to register all external classes to the class DB (we probably don't want this for common DataStructures, but rather Nodes)
+	printf("[CESIUM] Registering classes...\n");
+	fflush(stdout);
 	ClassDB::register_class<CesiumGeoreference>();
+	printf("[CESIUM] registered CesiumGeoreference\n"); fflush(stdout);
 	ClassDB::register_class<Cesium3DTileset>();
+	printf("[CESIUM] registered Cesium3DTileset\n"); fflush(stdout);
 	ClassDB::register_class<CesiumHTTPRequestNode>();
 	ClassDB::register_class<CesiumDebugUtils>();
 	ClassDB::register_class<CesiumGDPanel>();
@@ -45,8 +48,9 @@ void initialize_cesium_godot_module(ModuleInitializationLevel p_level) {
 	ClassDB::register_class<TokenTroubleshooting>();
 	ClassDB::register_class<GeoreferencedMesh>();
 	ClassDB::register_class<Cesium3DTile>();
-
 	ClassDB::register_class<CesiumGDCreditSystem>(true);
+	printf("[CESIUM] All classes registered\n");
+	fflush(stdout);
 }
 
 void uninitialize_cesium_godot_module(ModuleInitializationLevel p_level) {
@@ -56,12 +60,24 @@ void uninitialize_cesium_godot_module(ModuleInitializationLevel p_level) {
 #if defined(CESIUM_GD_EXT)
 extern "C" {
 	GDExtensionBool GDE_EXPORT test_cesium_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization* r_initialization){
-		printf("%s", "Initialization of GDExtension");
+		printf("[CESIUM] test_cesium_init entered\n");
+		fflush(stdout);
 		godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+		printf("[CESIUM] InitObject created\n");
+		fflush(stdout);
 		init_obj.register_initializer(initialize_cesium_godot_module);
+		printf("[CESIUM] initializer registered\n");
+		fflush(stdout);
 		init_obj.register_terminator(uninitialize_cesium_godot_module);
+		printf("[CESIUM] terminator registered\n");
+		fflush(stdout);
 		init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
-		return init_obj.init();
+		printf("[CESIUM] calling init_obj.init()\n");
+		fflush(stdout);
+		GDExtensionBool result = init_obj.init();
+		printf("[CESIUM] init returned: %d\n", result);
+		fflush(stdout);
+		return result;
   }
 }
 #endif
